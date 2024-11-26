c9ae0e39ea089bf5b6fc05452c0504a87bf5a846
edwinvarghese15 <127734453+edwinvarghese15@users.noreply.github.com>
2024-11-26 10:36:11 +0530

    Update dfs.c

--- dfs.c
+++ dfs.c
@@ -1,99 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
-struct Node 
+#include <stdbool.h>
+#define MAX 6
+int vertex_count =0;
+struct vertex
 {
-    int dest;
-    struct Node* next;
+	char data;
+	bool visited;
 };
 
-
-struct AdjList 
+struct vertex *graph[MAX];
+int adj_matrix[MAX][MAX];
+int stack[MAX];
+int top = -1;
+void push(int data)
 {
-    struct Node* head;
-};
-
-
-struct Node* createNode(int dest) 
-{
-    struct Node* newNode =
-      (struct Node*)malloc(sizeof(struct Node));
-    newNode->dest = dest;
-    newNode->next = NULL;
-    return newNode;
+	stack[++top]=data;
 }
-
-
-void DFSRec(struct AdjList adj[], int visited[], int s) 
+int pop()
 {
-   
-    visited[s] = 1;
-
-    
-    printf("%d ", s);
-
-    
-    struct Node* current = adj[s].head;
-    while (current != NULL) 
-    {
-        int dest = current->dest;
-        if (!visited[dest]) 
-        {
-            DFSRec(adj, visited, dest);
-        }
-        current = current->next;
-    }
+	return stack[top--];
 }
-
-
-void DFS(struct AdjList adj[], int V, int s)
+int peek()
 {
-   
-    int visited[5] = {0}; 
-    DFSRec(adj, visited, s);
+	return stack[top];
 }
-
-
-void addEdge(struct AdjList adj[], int s, int t) 
+bool is_stack_empty()
 {
-   
-    struct Node* newNode = createNode(t);
-    newNode->next = adj[s].head;
-    adj[s].head = newNode;
-
- 
-    newNode = createNode(s);
-    newNode->next = adj[t].head;
-    adj[t].head = newNode;
+	return top == -1;
 }
-
-int main() 
+void add_vertex(char data)
 {
-    int V = 5;
-
-
-    struct AdjList adj[V];
-
-   
-    for (int i = 0; i < V; i++)
-        {
-        adj[i].head = NULL;
-    }
-    
-    int E = 5;
-   
-    int edges[][2] = {{1, 2}, {1, 0}, {2, 0}, {2, 3}, {2, 4}};
-
-   
-    for (int i = 0; i < E; i++) 
-    {
-        addEdge(adj, edges[i][0], edges[i][1]);
-    }
-
-    int source = 1; 
-    printf("DFS from source: %d\n", source);
-    DFS(adj, V, source);
-
-    return 0;
+	struct vertex *new = (struct vertex*)malloc(sizeof(struct vertex));
+	new->data = data;
+	new->visited = false;
+	graph[vertex_count]=new;
+	vertex_count++;
+}
+void add_edge(int start,int end)
+{
+	adj_matrix[start][end]=1;
+	adj_matrix[end][start]=1;
+}
+int adj_vertex(int vertex_get)
+{
+	int i;
+	for(i=0;i<vertex_count;i++)
+	{
+		if(adj_matrix[vertex_get][i] == 1 && graph[i]->visited == false){
+			return i;
+		}
+	}
+	return -1;
+}
+void display_vertex(int pos)
+{
+	printf("%c",graph[pos]->data);
+}
+void dfs()
+{
+	int i;
+	int unvisited;
+	printf("\n|||||||||||||||||||||||||||||||\n");
+	graph[0]->visited =true;
+	display_vertex(0);
+	push(0);
+	
+	while(!is_stack_empty()){
+		int unvisited = adj_vertex(peek());
+		
+	    if(unvisited == -1){
+	    	pop();
+	    }
+	    else{
+	    	graph[unvisited]->visited = true;
+			display_vertex(unvisited);
+			push(unvisited);
+	    }
+	}
+	printf("\n|||||||||||||||||||||||||||||||\n");
+	for(i=0;i<vertex_count;i++){
+		graph[i]->visited = false;
+	}
+}
+void show(){
+	int i;
+	printf("\n.................................\n");
+	for(i=0;i<vertex_count;i++){
+		printf("Edge postion of '%c' is %d\n",graph[i]->data,i);
+	}
+	printf(".................................\n");
+}
+int main(){
+	int opt;
+	char data;
+	int edge_1,edge_2;
+	int i, j;
+    for(i = 0; i < MAX; i++)  
+      for(j = 0; j < MAX; j++) 
+         adj_matrix[i][j] = 0;
+	do{
+		printf("\n1)Add vertex \n2)Create edge \n3)Traversal \n0)Exit \nChoose option :: ");
+		scanf("%d",&opt);
+		switch(opt){
+			case 1:
+				printf("\nEnter data to be added to vertex : ");
+				scanf(" %c", &data);
+				add_vertex(data);
+				break;
+			case 2:
+	         	show();
+				printf("\nEnter edge starting : ");
+				scanf("%d",&edge_1);
+				printf("\nEnter edge ending : ");
+				scanf("%d",&edge_2);
+				if(vertex_count-1 < edge_1 || vertex_count-1 < edge_2){
+					printf("\nThere is no vertex !!\n");
+				}
+				else{
+					add_edge(edge_1,edge_2);
+				}
+				break;
+			case 3:
+				dfs();
+				break;
+			default:
+				printf("\nInvalid option try again !! ...");
+		}
+	}while(opt!=0);	
+	return 0;
 }
